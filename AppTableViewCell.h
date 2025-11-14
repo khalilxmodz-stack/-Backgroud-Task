787bc1aee31b3afcfc6be97fb039340cf801cd7a
khalilxmodz-stack <khalilxmodz@gmail.com>
2025-11-15 00:12:00 +0100

    Add files via upload

---
+++ AppTableViewCell.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface AppTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) UILabel *nameLabel;
+@property (nonatomic, strong) UILabel *statusLabel;
+
+@end
